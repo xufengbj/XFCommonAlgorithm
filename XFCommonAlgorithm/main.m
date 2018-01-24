373f9623078d7102e77bdb8f4a073618ecfc3218
许锋 <1192936115@qq.com>
2018-01-24 18:20:36 +0800

    Initial Commit

---
+++ XFCommonAlgorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XFCommonAlgorithm
+//
+//  Created by 许锋 on 2018/1/24.
+//  Copyright © 2018年 许锋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
